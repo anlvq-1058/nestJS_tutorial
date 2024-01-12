417670fdacd0f43dd4a7eb14133fdb4ff050d6fc
LVQA <quanganlevan@gmail.com>
2024-01-12 09:46:24 +0700

    part 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksController } from './tasks/tasks.controller';
 
 @Module({
   imports: [TasksModule],
-  controllers: [AppController, TasksController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/tasks/dto/create-task.dto.ts/create-task.dto.ts.ts
@@ -0,0 +1,4 @@
+export class CreateTaskDtoTs {
+  title: string;
+  description: string;
+}
